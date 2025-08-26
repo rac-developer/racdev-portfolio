36019b85e77ca08ce88211ebffa7931b16348d0e
Rodolfo Castro <soyracm@gmail.com>
2025-08-26 14:26:28 -0400

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter  } from "next/font/google";
 import "../ui/globals.css";
+import { Header } from "@/components/Header";
 
 const primaryFont = Inter ({
   variable: "--font-inter-sans",
@@ -22,6 +23,9 @@ export default function RootLayout({
       <body
         className={`${primaryFont.variable} min-h-svh overflow-x-hidden`}
       >
+        <div className="sticky top-0 z-50 py-4 -mx-4 xl:-mx-8 px-4 xl:px-8 mb-4">
+        <Header/>
+      </div>
         {children}
       </body>
     </html>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import React, { useEffect, useState } from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
+// import { Link } from "@heroui/link";
+// import { Button } from "@heroui/button";
+// import Image from "next/image";
+import Logotipo from "@/components/Logotipo";
 
 export const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header>
-      <Image
-        src="logotipo.svg"
-        width={500}
-        height={500}
-        alt="racdeveloper"
-      />
-    </header>
-  )
-}
+    <Navbar
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
+        scrolled
+          ? "backdrop-blur-md h-16 shadow-md"
+          : "bg-transparent h-20"
+      }`}
+    >
+      <NavbarBrand className="pl-6">
+        <Logotipo className="transition-all duration-300 text-secundary" width={160} />
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem className="pr-6">
+          {/* <Button as={Link} color="primary" href="#" variant="flat">
+            Idioma
+          </Button> */}
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
